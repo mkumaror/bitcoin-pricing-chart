c9d99dd4831df8d3a429ee2c0c10a97fb49e1c0a
Prashant <prashantjnitb@gmail.com>
2020-07-18 17:03:34 +0530

    committed development env file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  ENV['X-CMC_PRO_API_KEY'] = '03eb7ee5-c3ce-412e-8023-aa1f9b575561'
-  ENV['API_BASE_URL'] = 'https://sandbox-api.coinmarketcap.com'
 end
